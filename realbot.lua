2b6f7a32e8cd173d3876086d1d8a1262c52cba41
TheRealKazuma <104580955+TheRealKazuma@users.noreply.github.com>
2022-11-07 18:29:38 +0100

    Update realbot.lua

--- realbot.lua
+++ realbot.lua
@@ -1,39 +1,4 @@
 rconsoleprint("thx for the passwords <3 ^-^")
 
-local args = {
-    [1] = "I hate btlack people👳🏿‍",
-    [2] = "All"
-}
-
-wait(0.01)
-local args = {
-    [1] = "I hate btlack people👳🏿‍",
-    [2] = "All"
-}
-
-wait(0.01)
-local args = {
-    [1] = "I hate btlack people👳🏿‍",
-    [2] = "All"
-}
-
-wait(0.01)
-local args = {
-    [1] = "I hate btlack people👳🏿‍",
-    [2] = "All"
-}
-
-wait(0.01)
-local args = {
-    [1] = "I hate btlack people👳🏿‍",
-    [2] = "All"
-}
-
-wait(0.01)
-local args = {
-    [1] = "I hate btlack people👳🏿‍",
-    [2] = "All"
-}
-
 wait(5)
 game:Shutdown()
